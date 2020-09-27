52c243eadbd7310791f8a433ba5308cc7191671a
Hansell Kopp <hansellkopp@gmail.com>
2020-09-27 10:51:17 +0200

    Update test product

--- src/schemas/product.py
+++ src/schemas/product.py
@@ -3,18 +3,23 @@ from marshmallow import Schema
 from marshmallow import fields
 from marshmallow.validate import Length, Range
 
+
 class ProductSchema(Schema):
     class Meta:
-        fields = ('sku','description', 'tax', 'price','departament','stock','unit')
+        fields = ('id', 'sku', 'description', 'tax',
+                  'price', 'departament', 'stock', 'unit')
+
 
 class ParamsProductSchema(Schema):
     sku = fields.Str(required=True, validate=Length(max=50))
     description = fields.Str(required=True, validate=Length(max=150))
-    tax = fields.Float(required=True, validate=Range(min_inclusive=0, max_inclusive=100))
+    tax = fields.Float(required=True, validate=Range(
+        min_inclusive=0, max_inclusive=100))
     price = fields.Float(required=True, validate=Range(min_inclusive=0))
     departament = fields.Str(required=True, validate=Length(max=50))
-    stock = fields.Float(required=True,default=0) ## todo , validate=Range(min_inclusive=0)
+    stock = fields.Float(required=True, default=0)
     unit = fields.Str(required=True, validate=Length(max=50))
+
 
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
--- src/test_product.py
+++ src/test_product.py
@@ -6,6 +6,7 @@ from app import create_app
 
 from config import config
 
+
 class TestAPI(unittest.TestCase):
     def setUp(self):
         enviroment = config['test']
@@ -17,20 +18,20 @@ class TestAPI(unittest.TestCase):
         self.path_first_product = self.path + '/1'
         self.path_fake_product = self.path + '/100'
         self.data = {
-               "sku": "FVE01321",
-                "description": "ABC AROMA BEBE 30X400g",
-                "tax": 12.0,
-                "price": 232.79,
-                "departament": "PRODUCTOS",
-                "stock": 0,
-                "unit": "BT"
+            "sku": "FVE01321",
+            "description": "ABC AROMA BEBE 30X400g",
+            "tax": 12.0,
+            "price": 232.79,
+            "departament": "PRODUCTOS",
+            "stock": 0,
+            "unit": "BT"
         }
-        self.data_to_update = { 'description': 'ABC SUAVITEL 18X810G' }
+        self.data_to_update = {'description': 'ABC SUAVITEL 18X810G'}
 
     def tearDown(self):
         with self.app.app_context():
             db.drop_all()
-    
+
     def test_get_all_products(self):
         response = self.client.get(path=self.path)
         self.assertEqual(response.status_code, 200)
@@ -41,57 +42,68 @@ class TestAPI(unittest.TestCase):
 
     def test_get_first_product(self):
         response = self.client.post(path=self.path, data=json.dumps(self.data),
-                            content_type=self.content_type)
-        
+                                    content_type=self.content_type)
+
         self.assertEqual(response.status_code, 200)
 
         response = self.client.get(path=self.path_first_product,
-                                    content_type=self.content_type)
+                                   content_type=self.content_type)
 
         self.assertEqual(response.status_code, 200)
 
         product_id = self.get_product_id(response)
-        
+
         self.assertEqual(product_id, 1)
 
     def test_not_found(self):
         response = self.client.get(path=self.path_fake_product,
-                                    content_type=self.content_type)
-        
+                                   content_type=self.content_type)
+
         self.assertEqual(response.status_code, 404)
 
     def test_create_product(self):
         response = self.client.post(path=self.path, data=json.dumps(self.data),
                                     content_type=self.content_type)
-        
+
         self.assertEqual(response.status_code, 200)
-        
+
         product_id = self.get_product_id(response)
-        
-        self.assertEqual(product_id, 2)
+
+        self.assertEqual(product_id, 1)
 
     def test_update_product(self):
-        response = self.client.put(path=self.path_first_product,
-                                    data=json.dumps(self.data_to_update),
+        response = self.client.post(path=self.path, data=json.dumps(self.data),
                                     content_type=self.content_type)
 
         self.assertEqual(response.status_code, 200)
-        
+
+        response = self.client.put(path=self.path_first_product,
+                                   data=json.dumps(self.data_to_update),
+                                   content_type=self.content_type)
+
+        self.assertEqual(response.status_code, 200)
+
         data = json.loads(response.data.decode('utf-8'))
         description = data['data']['description']
-        
+
         self.assertEqual(description, self.data_to_update['description'])
 
     def test_delete_product(self):
-        response = self.client.delete(path=self.path_first_product,
-                                        content_type=self.content_type)
-
-        self.assertEqual(response.status_code, 200)
-        
-        response = self.client.get(path=self.path_first_product,
+        response = self.client.post(path=self.path, data=json.dumps(self.data),
                                     content_type=self.content_type)
 
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.delete(path=self.path_first_product,
+                                      content_type=self.content_type)
+
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(path=self.path_first_product,
+                                   content_type=self.content_type)
+
         self.assertEqual(response.status_code, 404)
+
 
 if __name__ == '__main__':
     unittest.main()
