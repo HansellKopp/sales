6e91da903aa8faee084be1d1498d55f0e82a8e31
Hansell Kopp <hansellkopp@gmail.com>
2021-01-03 21:03:58 +0100

    build

--- react-app/src/components/Products/index.jsx
+++ react-app/src/components/Products/index.jsx
@@ -4,7 +4,10 @@ import ProductList from 'components/Products/ProductList'
 
 const Products = () => {
     const products = useSelector(state => state.product.products)
-    const selected = useSelector(state => state.state.selectedGroup)
+    let selected = useSelector(state => state.state.selectedGroup)
+    if(!selected && products[0]) {
+        selected = products[0].departament
+    }
     const filtered = products.filter(product=> product.departament === selected)
     return (
         <div className="w-full">
