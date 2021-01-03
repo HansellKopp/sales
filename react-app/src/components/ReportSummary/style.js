ea8dd4021b91a46eaf45cd78dd67a875c0da0a9c
Hansell Kopp <hansellkopp@gmail.com>
2021-01-03 12:41:02 +0100

    Update summary styles

--- react-app/src/components/ReportSummary/style.js
+++ react-app/src/components/ReportSummary/style.js
@@ -1,4 +1,4 @@
-import { makeStyles, lighten } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,28 +32,6 @@ const useStyles = makeStyles((theme) => ({
       padding: '40px 20px'
     }
 }))
-
-const useToolbarStyles = makeStyles((theme) => ({
-    root: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(1), 
-
-    },
-    highlight:
-      theme.palette.type === 'light'
-        ? {
-            color: theme.palette.secondary.main,
-            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-          }
-        : {
-            color: theme.palette.text.primary,
-            backgroundColor: theme.palette.secondary.dark,
-          },
-    title: {
-      flex: '1 1 100%',
-    }
-}))
-
 const useHeadStyles = makeStyles((theme) => ({
   root: {
     paddingLeft: theme.spacing(2),
@@ -66,6 +44,5 @@ const useHeadStyles = makeStyles((theme) => ({
 
 export {
   useStyles,
-  useHeadStyles,
-  useToolbarStyles
+  useHeadStyles
 }
